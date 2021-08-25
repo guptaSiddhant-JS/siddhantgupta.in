a76476cfb152474805ccab8fea10d4617d1308f8
Siddhant Gupta <siddhant.11901864@lpu.in>
2021-08-25 09:26:04 +0530

    Added for testing

--- src/components/pages/education/Education.js
+++ src/components/pages/education/Education.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Card from '../../Card/Card';
 import styles from './Education.module.css'
 const Education = () =>{
   return (
-      <div className={styles.container}>
+
+      <Card  className={styles.container}>
          Education
-      </div>
+      </Card>
   );
 }
 export default Education;
