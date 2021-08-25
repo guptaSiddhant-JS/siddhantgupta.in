80978afc5cfd77868604a42c1c5bfc7e9ae827e9
Siddhant Gupta <siddhant.11901864@lpu.in>
2021-08-25 15:15:45 +0530

    Added Card Default Card Outlet

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,13 +3,16 @@ import styles from './Card.module.css';
 const Card = props =>{
    return (
        <div className={styles.container}>
+          
           <ul className={styles.icon}>
-           <li></li>
+           <li><img src='about.png' /></li>
           </ul>
+          
           <div
            className={`${styles.card} ${props.className}`} 
            style={props.style}
           >
+             <h2 className={styles.title}>About</h2>
           {props.children}
          </div>
        </div>
