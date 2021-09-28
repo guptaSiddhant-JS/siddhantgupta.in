f8f668e308cb8b4859495f450f4505acedeb7879
Siddhant Gupta <siddhant.11901864@lpu.in>
2021-09-28 19:35:15 +0530

    Updated the content Page

---
+++ src/pages/ContentU/Content.js
@@ -0,0 +1,9 @@
+ import ContentCss from './Content.module.css'
+ const Content = props =>{
+ return (
+     <div className={ContentCss.container}>
+         Content Page (30%)
+     </div>
+ );
+ }
+ export default Content;
